4cfb10ffff742f5f1f2fcf5c82568440c93cf1d2
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2016-04-23 16:27:48 +0300

    added zoom of image

--- ShowCats/PhotoDetailedViewController.swift
+++ ShowCats/PhotoDetailedViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class PhotoDetailedViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     var photo:Photo!
     
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Для зума нужны 2 момента:
+        //1. Задать разные иминимальный и максимальный размеры для масштаба (zoom)
+        scrollView.minimumZoomScale = 0.7
+        scrollView.maximumZoomScale = 4
+        //2. Нужно стать делегатом scrollView и переопределить метод
+        // viewForZooming у скроллвью
+        scrollView.delegate = self
+        
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -23,5 +32,12 @@ class PhotoDetailedViewController: UIViewController {
                                      placeholderImage: nil,
                                      options: [.ProgressiveDownload])
     }
+}
 
+//Чтобы заработало масштабирование, нужно переопределить 
+//viewForZoomingInScroolView
+extension PhotoDetailedViewController: UIScrollViewDelegate {
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
